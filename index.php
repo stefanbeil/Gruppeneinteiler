8847dd905631b1e1cbc335bd06952a098b026aae
stefanbeil <72492445+stefanbeil@users.noreply.github.com>
2020-10-12 10:37:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 
 <?php // Seite mit "schnellerer" Eingabemaske
 require_once __DIR__ . "/src/controller/MasterController1.php";
+require_once __DIR__ . "/src/model/findBest/FindBestAll1.php";
+
 
 $MAX_QUANTITY_PERSONS = 100;
 $MAX_GROUP_SIZE = 20;
 $MAX_QUANTITY_GROUPS = 20;
 $MAX_COUNT_ROUNDS = 20;
+
 
 $mastercontroller1 = new MasterController1();
 
@@ -17,46 +20,9 @@ if (isset($_GET['quantityPersons']) && isset($_GET['groupSize'])
   $quantityGroups = min(intval(htmlspecialchars($_GET['quantityGroups'])), $MAX_QUANTITY_GROUPS);
   $countRounds = min(intval(htmlspecialchars($_GET['rounds'])), $MAX_COUNT_ROUNDS);
 
-  list($bestRounds, $bestPersons, $newEncountersPerPersonPerRound) =
-    $mastercontroller1->findBestResult($quantityPersons, $groupSize,
-     $quantityGroups, $countRounds);
-
-  renderResults($mastercontroller1, $bestRounds, $bestPersons, $newEncountersPerPersonPerRound,
-    $quantityPersons, $groupSize, $quantityGroups, $countRounds);
+  $mastercontroller1->renderResults($quantityPersons, $groupSize, $quantityGroups, $countRounds);
 } else {
-  renderNoResults($mastercontroller1);
-}
-
-
-function renderResults($mastercontroller1, $bestRounds, $bestPersons, $newEncountersPerPersonPerRound,
-  $quantityPersons, $groupSize, $quantityGroups, $countRounds)
-{
-  $mastercontroller1->render("top.php", []);
-  $mastercontroller1->render("parameters1.php", [
-    "quantityPersons" => $quantityPersons,
-    "groupSize" => $groupSize,
-    "quantityGroups" => $quantityGroups,
-    "rounds" => $countRounds
-  ]);
-  $mastercontroller1->render("statistics.php", [
-    "bestRounds" => $bestRounds,
-    "bestPersons" => $bestPersons,
-    "rounds" => $countRounds,
-    "newEncountersPerPersonPerRound" => $newEncountersPerPersonPerRound
-   ]);
- $mastercontroller1->render("rounds.php", [
-   "bestRounds" => $bestRounds,
-   "bestPersons" => $bestPersons,
-   "rounds" => $countRounds
-  ]);
-}
-
-function renderNoResults($mastercontroller1)
-{
-  $mastercontroller1->render("top.php", []);
-  $mastercontroller1->render("parameters1.php", []);
-  $mastercontroller1->render("noStatistics.php", []);
-  $mastercontroller1->render("noRounds.php", []);
+  $mastercontroller1->renderNoResults();
 }
 
  ?>
