5c52f3170a8cf0a7deb348d1bd1696234ff7db0c
stefanbeil <72492445+stefanbeil@users.noreply.github.com>
2020-10-12 10:36:57 +0200

    Update MasterController2.php

--- src/controller/MasterController2.php
+++ src/controller/MasterController2.php
@@ -4,68 +4,49 @@ include_once __Dir__ . "/../model/MeetingRoom.php";
 include_once __Dir__ . "/../model/Meeting.php";
 include_once __Dir__ . "/../model/Round.php";
 include_once __Dir__ . "/AbstractController.php";
+include_once __Dir__ . "/../model/findBest/FindBestAll2.php";
 
-
-class MasterConroller2 extends AbstractController
+class MasterController2 extends AbstractController
 {
   public function __construct()
   {
   }
-  /*siehe Kommentare bei findBestResult
-  in MasterController1.php für mehr Details*/
-  public function findBestResult(array $people, array $rooms, $countRounds)  //genauere Eingabemaske
+
+  function renderResults($people, $rooms, $countRounds)
   {
-    $roundsSearchDepth = 1;
-    $meetingsSearchDepth = 1;
-    $cycles = 0;
+    $fba = new FindBestAll2();
+    list($bestRounds, $bestPersons, $newEncountersPerPersonPerRound) =
+      $fba->findBestResult($people, $rooms, $countRounds);
 
-    $bestTotalCount = 0;
-    $bestRounds = array();
-    $bestPersons = array();
-
-    $meetingRooms = $this->createDifferentSizedMeetingRooms($rooms);//$rooms enthölt die Größe der einzelnen Räume
-
-    $beginn = microtime(true);
-    do {
-      $persons = $this->createPersonsWithName($people);
-      $rounds = $this->findGoodRounds($countRounds, $persons, $meetingRooms, $roundsSearchDepth, $meetingsSearchDepth);
-      //statische Zählervariablen zurücksetzen
-      Meeting::$meetingName = 1;
-      $roundCount = $this->totalCount($persons);
-      if ($roundCount > $bestTotalCount) {
-        $bestTotalCount = $roundCount;
-        $bestRounds = $rounds;
-        $bestPersons = $persons;
-      }
-      $cycles++;
-      $meetingsSearchDepth++;
-      $roundsSearchDepth = floor($meetingsSearchDepth / 10) + 1;
-      $totalDuration = microtime(true) - $beginn;
-    } while($totalDuration < 1.5);
-
-    $this->logInfoToConsole($totalDuration, $cycles);
-    $newEncountersPerPersonPerRound = $this->newEncountersPerPersonPerRound($bestRounds);
-    return array($bestRounds, $bestPersons, $newEncountersPerPersonPerRound);
+    $this->render("top.php", []);
+    $this->render("parameters2.php", [
+        "bestRounds" => $bestRounds,
+        "bestPersons" => $bestPersons,
+        "people" => $people,
+        "rooms" => $rooms,
+        "rounds" => $countRounds
+     ]);
+     $this->render("statistics.php", [
+       "bestRounds" => $bestRounds,
+       "bestPersons" => $bestPersons,
+       "rounds" => $countRounds,   //zu countRounds umbenennen
+       "newEncountersPerPersonPerRound" => $newEncountersPerPersonPerRound
+      ]);
+    $this->render("rounds.php", [
+      "bestRounds" => $bestRounds,
+      "bestPersons" => $bestPersons,
+      "rounds" => $countRounds   //zu countRounds umbenennen
+     ]);
   }
 
-  private function createDifferentSizedMeetingRooms($rooms)
+  function renderNoResults()
   {
-    $meetingRooms = array();
-    for ($i = 0; $i < count($rooms); $i++) {
-      $meetingRooms[] = new MeetingRoom($rooms[$i]);
-    }
-    return $meetingRooms;
-  }
-
-  private function createPersonsWithName($people)
-  {
-    $persons = array();
-    for ($i = 0; $i < count($people); $i++) {
-      $person = new Person($people[$i]);
-      $persons[] = $person;
-    }
-    return $persons;
+    $this->render("top.php", []);
+    $this->render("parameters2.php", []);
+    $this->render("noStatistics.php", []);
+    $this->render("noRounds.php", []);
   }
 
 }
-?>
+
+ ?>
